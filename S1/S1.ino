8b6f079082ebc69578420f02b9b723f802ca7fbd
João Medeiros <joao_m_medeiros@estudante.sesisenai.org.br>
2025-10-27 10:19:15 -0300

    envio e recebimento de mensagens

--- S1/S1.ino
+++ S1/S1.ino
@@ -4,43 +4,67 @@
 WiFiClient client;
 PubSubClient mqtt(client);
 
-const String SSID = "FIESC_IOT_EDU";
-const String PASS = "8120gv08";
+#define PINO_LED 2
 
 //constantes p/ broker
 const String URL = "test.mosquitto.org";
 const int PORT = 1883;
 const String USR = "";
 const String broker_PASS = "";
-const String Topic = "DSM1";
+const String MyTopic = "Joao";
+const String OtherTopic = "Daniel";
 
 
-void setup()  {
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+void setup() {
   Serial.begin(115200);
-  Serial.println("Conectando ao WiFi");
+  Serial.print("Conectado ao Wi-Fi");
   WiFi.begin(SSID,PASS);
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
   }
-  Serial.println("\nConectado com sucesso!");
-  Serial.println("Conectando ao Broker");
+  Serial.print("\nConectado com sucesso!");
+  Serial.print("Conectando ao broker");
   mqtt.setServer(URL.c_str(),PORT);
   while(!mqtt.connected()){
     String ID = "S1_";
-    ID += String(random(0xffff),HEX); //cria a parte aleat.
+    ID += String(random(0xffff),HEX);
     mqtt.connect(ID.c_str(),USR.c_str(),broker_PASS.c_str());
     Serial.print(".");
     delay(200);
   }
-  Serial.println("\nConectado com sucesso ao broker!");
+  pinMode(PINO_LED, OUTPUT);
+  mqtt.subscribe(MyTopic.c_str());
+  mqtt.setCallback(callback);
+  Serial.println("\nConecxao com sucesso ao broker!");
 }
 
 void loop() {
-  String mensagem = "Joao: ";
-  mensagem += "alguma coisa";
-
-  mqtt.publish(Topic.c_str(),mensagem.c_str());
+  String mensagem ="Joao: ";
+  if(Serial.available()>0){
+    mensagem += Serial.readStringUntil('\n');
+    mqtt.publish(OtherTopic.c_str(),mensagem.c_str());
+  }
   mqtt.loop();
   delay(1000);
+
+}
+
+void callback(char* topic, byte* payload, unsigned int length){
+  String mensagem = "";
+  for(int i = 0; i < length; i++){
+    mensagem += (char)payload[i];//acender led
+  }
+  Serial.print("Recebido: ");
+  Serial.println(mensagem);
+  if(mensagem == "Daniel: Acender"){
+    digitalWrite(PINO_LED, HIGH);
+  }else if(mensagem == "Daniel: Apagar"){
+    digitalWrite(PINO_LED, LOW);
+  }else{
+    Serial.println(mensagem);
+  }
 }
